036f3c4790fa6c04005128d2b9f85562f801ebb2
Patricio Monpelat <patricio.monpelat@gmail.com>
2025-04-24 11:26:05 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const letter = document.getElementById("changing-letter");
+  const options = ["o", "a"];
+  let index = 0;
+
+  setInterval(() => {
+    index = (index + 1) % options.length;
+    letter.classList.add("opacity-0");
+    setTimeout(() => {
+      letter.textContent = options[index];
+      letter.classList.remove("opacity-0");
+    }, 300);
+  }, 2500);
+});
