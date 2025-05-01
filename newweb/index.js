cbad3d4ccfc26344607bc7585b2a134c6da2e8ed
Patricio Monpelat <patricio.monpelat@gmail.com>
2025-05-01 20:58:41 -0300

    .

--- newweb/index.js
+++ newweb/index.js
@@ -133,3 +133,9 @@ document
   }
   // Dejas que el form siga su curso (POST a Formspree)
 });
+
+document.querySelector('.btn-reset').addEventListener('click', () => {
+  document
+    .querySelectorAll('.buscador-avanzado select')
+    .forEach(sel => sel.selectedIndex = 0);
+});
